0bf3105f875981b32449ba39add466d9684f907f
Sohail Momin <zelan9691@gmail.com>
2024-07-25 21:51:10 +0530

    counter app

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import Counter from './components/counter'
+import { useState } from "react";
+import CounterList from "./components/CounterList";
 
 function App() {
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
+
+  const [data, setData] = useState([]);
+
+  const submitData = () => {
+    let object = {
+      name,
+      color,
+    };
+    setData([...data,object]);
+    setName("")
+    setColor("")
+  };
+
+  console.log(data);
+
   return (
-    <div>
-      <Counter/>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        border: "1px solid black",
+        margin: "auto",
+      }}
+    >
+      <div
+        style={{
+          padding: "10px",
+          border: "1px solid black",
+          margin: "auto",
+        }}
+      >
+        <CounterList userData={data} />
+      </div>
+      <div
+        style={{
+          padding: "10px",
+          border: "1px solid black",
+          margin: "auto",
+        }}
+      >
+        <input
+        value={name}
+          placeholder="enter name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+         value={color}
+          placeholder="enter color"
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <button
+          onClick={submitData}
+          style={{
+            color: "white",
+            background: "black",
+          }}
+        >
+          submit
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
