5ae1167dfae4221e4057b6c9a6b5831dc4196cfc
Sohail Momin <zelan9691@gmail.com>
2024-07-28 16:43:20 +0530

    app

--- second-counter-app/src/App.jsx
+++ second-counter-app/src/App.jsx
@@ -1,32 +1,59 @@
-import Counter from "./components/counter";
-import { useState } from "react";
-//1:first u will have to import the component//
-//and use the component inside the return(<counter/>)which is in app function//
-//2:then make a input tag
+import CounterList from "./components/counter-list"; // Importing CounterList component
+import { useState } from "react"; // Importing useState hook from React
 
 const App = () => {
-  //3:set and update the color
-  //set and update the name
-  //set and update the list i.e array of object//
-  const [color, setColor] = useState("");
-  const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  // Step 1: Set up state for the form inputs and the list of counters
+  const [color, setColor] = useState(""); // State to store color input
+  const [name, setName] = useState(""); // State to store name input
+  const [list, setList] = useState([]); // State to store list of counters
 
+  // Step 2: Handle adding a new counter to the list
+  const handleAddCounter = () => {
+    // Check if both name and color are provided
+    if (name && color) {
+      // Update the list state with the new counter
+      setList([...list, { name, color }]);
+      // Reset the input fields
+      setName("");
+      setColor("");
+    } else {
+      // Alert if name or color is missing
+      alert("Please enter both name and color");
+    }
+  };
+
+
+
+  // Step 3: Render the UI
   return (
     <div>
-      <Counter />
+      <CounterList list={list} /> {/* Pass the list state to CounterList component */}
       <div
         style={{
-          marginTop: "10px",
-          padding: "10px",
-          border: "2px solid blue",
+          display: "flex", // Display inputs and button in a flexbox
+          gap: "10px", // Space between input fields and button
+          marginTop: "10px", // Top margin for some spacing
+          padding: "10px", // Padding inside the div
+          border: "2px solid blue", // Blue border around the div
         }}
       >
-        <input placeholder="enter name" />
-        <input placeholder="enter color" />
+        {/* Input for name */}
+        <input
+          placeholder="Enter name" // Placeholder text
+          value={name} // Bind the input value to name state
+          onChange={(e) => setName(e.target.value)} // Update the name state on input change
+        />
+        {/* Input for color */}
+        <input
+          placeholder="Enter color" // Placeholder text
+          value={color} // Bind the input value to color state
+          onChange={(e) => setColor(e.target.value)} // Update the color state on input change
+        />
+        {/* Button to add a new counter */}
+        <button onClick={handleAddCounter}>Submit</button> {/* Trigger handleAddCounter on click */}
       </div>
     </div>
   );
 };
 
-export default App;
+export default App; // Export the App component
--- second-counter-app/src/components/counter.jsx
+++ second-counter-app/src/components/counter.jsx
@@ -1,7 +1,7 @@
 //make a function//
 // from:line 38 import the hook before using it//
 import { useState } from "react";
-const Counter = () => {
+const Counter = ({ name, color }) => {
   // write a return statement then and then only it will show//
   // it will show the content u wrote inside it on the UI//
   //also set the initial for the hook and the updated value will be stored in the setCounter//
@@ -11,33 +11,43 @@ const Counter = () => {
   return (
     <div
       style={{
-        border: "2px solid red",
+        background: color,
         display: "flex",
         flexDirection: "column",
         margin: "auto",
         padding: "10px",
-      }}>
-
+      }}
+    >
       {/* for whom do we need the counter */}
       <h3
         style={{
           border: "2px solid green",
-        }}>Counter For:</h3>
-
+        }}
+      >
+        Counter For: {name}:
+      </h3>
 
       {/* show the actual count value */}
-    <div style={{border: "2px solid black",padding: "5px",}}>
+      <div style={{ border: "2px solid black", padding: "5px" }}>
         <h4>Count Value:{counter}</h4>
 
         {/* make the 2 buttons for increment and decrement respectively */}
-    <button
-      style={{width: "200px",}}onClick={() => setCounter(counter + 1)}>Inc</button>
+        <button
+          style={{ width: "200px" }}
+          onClick={() => setCounter(counter + 1)}
+        >
+          Inc
+        </button>
         {/* we have to add the onclick event on the button and invoke a function that will add 1 in the intial value and  for decrement it will minus 1 from the value*/}
-        <button style={{ width: "200px" }}onClick={() => setCounter(counter - 1)}>Dec</button>
+        <button
+          style={{ width: "200px" }}
+          onClick={() => setCounter(counter - 1)}
+        >
+          Dec
+        </button>
         {/* now we have to store a intial value and the updated value and update it as per the user requirements 
         so we will hook called useState() which stores the initial and final value*/}
       </div>
-      
     </div>
   );
 };
