3c2e41b6f1289f76504f78b9c49e93ae34cc855c
Sohail Momin <zelan9691@gmail.com>
2025-01-07 17:05:18 +0530

    check Boxes

--- MUI/src/App.jsx
+++ MUI/src/App.jsx
@@ -1,13 +1,15 @@
-import Buttons from "./Buttons"
-import BtnGroup from "./components/BtnGroup"
+import Buttons from "./Buttons";
+import CheckBox from "./Check-Box";
+import BtnGroup from "./components/BtnGroup";
 
 const App = () => {
   return (
     <div>
-      <Buttons/>
-      <BtnGroup/>
+      <Buttons />
+      <BtnGroup />
+      <CheckBox />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ MUI/src/Check-Box.jsx
@@ -0,0 +1,25 @@
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import { Checkbox } from "@mui/material";
+import { useState } from "react";
+const CheckBox = () => {
+  const [value, setValue] = useState([]);
+  const GetValue = (e) => {
+    const data = value;
+    data.push(e.target.value);
+    console.log(data)
+  };
+  return (
+    <div>
+      <Checkbox color="primary" value="sohail" onChange={(e) => GetValue(e)} />
+      <Checkbox color="primary" value="sufi" onChange={(e) => GetValue(e)} />
+      <Checkbox color="primary" value="azim" onChange={(e) => GetValue(e)} />
+      <Checkbox color="primary" value="kalim" indeterminate onChange={(e) => GetValue(e)}/>
+      <Checkbox color="secondary" value="mujju" onChange={(e) => GetValue(e)}
+      icon={<FavoriteBorder/>}
+      checkedIcon={<Favorite/>}
+       />
+    </div>
+  );
+};
+
+export default CheckBox;
