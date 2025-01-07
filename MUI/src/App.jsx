80a763207f90b481816b62e12682c831b0869baa
Sohail Momin <zelan9691@gmail.com>
2025-01-07 11:50:11 +0530

    buttons using mui

---
+++ MUI/src/App.jsx
@@ -0,0 +1,11 @@
+import Buttons from "./Buttons"
+
+const App = () => {
+  return (
+    <div>
+      <Buttons/>
+    </div>
+  )
+}
+
+export default App
---
+++ MUI/src/Buttons.jsx
@@ -0,0 +1,26 @@
+import { Button } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import { useState } from "react";
+const Buttons = () => {
+  const [color, setColor] = useState("primary");
+  //   const [disableBtn, setDisableBtn] = useState(false);
+
+  const CustomiseBtn = () => {
+    setColor("secondary");
+    // setDisableBtn(true);
+    alert("Button Clicked");
+  };
+  return (
+    <div>
+      <h1>Making Buttons Using MUI</h1>
+      <Button
+        color={color}
+        variant="contained"
+        // disabled={disableBtn}
+        onClick={() => CustomiseBtn()}
+        endIcon={<Delete />}>Click Here</Button>
+    </div>
+  );
+};
+
+export default Buttons;
